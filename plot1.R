575f42306db95cba30ca3a87492d6ce1e3e71fdb
FathimaAmara <fathimazuffer93@gmail.com>
2019-12-27 07:26:32 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 #Reading data
-data <- read.table("household_data_consumption.txt",skip=1,sep=";",na.strings="?")
-names(data) <- c("Date","Time","Global_active_data","Global_reactive_data","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+data <- read.table("household_power_consumption.txt",skip=1,sep=";",na.strings="?")
+names(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 subdata <- subset(data,data$Date=="1/2/2007" | data$Date =="2/2/2007")
 
 #plotting
