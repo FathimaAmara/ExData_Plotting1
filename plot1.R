eddf504557010240c6b4907fec88f61c5a2769dd
FathimaAmara <fathimazuffer93@gmail.com>
2019-12-27 06:35:19 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,10 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+#Reading data
+data <- read.table("household_data_consumption.txt",skip=1,sep=";",na.strings="?")
+names(data) <- c("Date","Time","Global_active_data","Global_reactive_data","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subdata <- subset(data,data$Date=="1/2/2007" | data$Date =="2/2/2007")
 
-#str(subSetData)
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-dev.off()
+#plot function
+hist(as.numeric(as.character(subdata$Global_active_data)),col="red",main="Global Active data",xlab="Global Active data(kilowatts)")
+
+# naming the graph
+title(main="Global Active Power")
